887ec8c5a8e8dfb805ed0d4f88822ce82bc7feb2
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-03-11 12:35:39 +0700

    Update layout.js

--- src/app/(homepages)/layout.js
+++ src/app/(homepages)/layout.js
@@ -1,21 +1,21 @@
 import { Providers } from "@/redux/Providers"
 
-import HeaderHomepage from "@/components/headers/HeaderHompages"
-import HeaderHomepages3 from "@/components/headers/HeaderHompages3"
-import Cart from "@/components/headers/Cart"
+// import HeaderHomepage from "@/components/headers/HeaderHompages"
+// import HeaderHomepages3 from "@/components/headers/HeaderHompages3"
+// import Cart from "@/components/headers/Cart"
 
 
 export default function HomepagesLayout({ children }) {
 
     return(
         <>
-            <Providers>
-                <HeaderHomepages3 />
+            {/* <Providers>
+                <HeaderHomepages3 /> */}
                 <main>
-                    <Cart />
+                    {/* <Cart /> */}
                     <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{ children }</div>
                 </main>
-            </Providers>
+            {/* </Providers> */}
         </>
     )
 }
